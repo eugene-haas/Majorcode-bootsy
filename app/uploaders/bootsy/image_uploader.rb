6b92b08334868dc1489a31eec650a7a428336f15
James Han <in1004kyu@gmail.com>
2016-11-16 11:43:17 +0900

    bootsy 업로드/압축 이미지 비율조절

--- app/uploaders/bootsy/image_uploader.rb
+++ app/uploaders/bootsy/image_uploader.rb
@@ -8,26 +8,29 @@ module Bootsy
       "#{Bootsy.store_dir}/#{model.class.to_s.underscore}/#{model.id}"
     end
 
-    process resize_to_limit: [1160, 2000]
+    process resize_to_limit: [974, 2000]
+
+    version :large do
+      process resize_to_fit: [487, 1000]
+    end
+
 =begin
     version :large do
       process resize_to_fit: [
         Bootsy.large_image[:width], Bootsy.large_image[:height]
       ]
     end
-
     version :medium do
       process resize_to_fit: [
         Bootsy.medium_image[:width], Bootsy.medium_image[:height]
       ]
     end
-=end
     version :small do
       process resize_to_fit: [
         Bootsy.small_image[:width], Bootsy.small_image[:height]
       ]
     end
-
+=end
     version :thumb do
       process resize_to_fill: [60, 60]
     end
